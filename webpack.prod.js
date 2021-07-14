1f93d12ae9c6f954c3bba3f467ed50e2dbc4e15b
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-07-14 15:35:17 +1000

    Add files via upload

---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                //use allows use to input an array of loaders
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
