3e2de0758a9d714e55495f42d91978db702cc2ab
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-07-16 12:40:16 +1000

    Update nameChecker.js

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -1,4 +1,4 @@
-function checkForName(inputText) {
+/*function checkForName(inputText) {
     console.log("::: Running checkForName :::", inputText);
     let names = [
         "Picard",
@@ -13,4 +13,19 @@ function checkForName(inputText) {
     }
 }
 
-export { checkForName }
+export { checkForName }*/
+
+function checkURL(inputText) {
+    console.log("Validating URL", inputText);
+
+    //this code is sourced from (as i was unsure how to validate urls): https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+    const url = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return url.test(inputText);
+}
+
+export { checkURL }
