af3b76c873d0c907b168ba5095819d8ad6b887be
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-07-24 17:13:37 +1000

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,8 @@
-import { checkURL } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { countdown } from "../countdown";
+import { retrieveDestination } from "../retrieveDestination";
+import { retrieveImage } from "../retrieveImage";
+import { retrieveWeather } from "../retrieveWeather";
+import { generateTrip } from '../js/app'
 
 import './styles/main.scss'
 import './styles/base.scss'
@@ -9,17 +12,42 @@ import './styles/header.scss'
 import './styles/slideshow.scss'
 import './styles.responsive.scss'
 
+let userData = {};
+
+//post data
+const userView = async()=>{
+      const entries = await fetch('/all');
+          //try
+          try{
+              const projectData = await entries.json();
+              //define variables
+              const destination = document.getElementById('destination').value;
+              const departure = document.getElementById('date').value;
+
+              // testing displaying geo API
+              const geoApiRes = await retrieveDestination(destination);
+
+              userData.lat = geoApiRes.lat;
+              console.log(travelData.lat);
+
+              userData.long = geoApiRes.long;
+              console.log(travelData.long);
+
+              userData.ctry = geoApiRes.ctry;
+            }
+          //catch any potential errors that arise and output results in console
+          catch(err){
+             console.log('Error posting data ' + err);
+      }}
+
 
 // Event listener to add function to existing HTML DOM element
 
 document.getElementById('generate').addEventListener("click", performAction);
 
-export {
- checkURL,
- handleSubmit
-}
+
 
 console.log(checkURL);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+//alert("I EXIST")
+//onsole.log("CHANGE!!");
