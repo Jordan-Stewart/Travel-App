eb4f8554219e4743abe19255e515f85bd4a5daa3
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-07-28 12:09:49 +1000

    setup of server.js file

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,9 @@
+//require other server js files
+const app = require("./index");
 
+// Initalize Server
+const server = app.listen(8080, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Server is running on http://${host}:${port}`);
+}
