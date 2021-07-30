3c9f6e11fbebcfba249acdd8b1f13d03fb3f8128
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-07-30 18:53:57 +1000

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -42,4 +42,32 @@ function generateTrip(e){
     //return zipCodePattern.test(elementValue);
   }
 
+  let userData = {};
+
+//post data
+const userView = async()=>{
+      const entries = await fetch('/all');
+          //try
+          try{
+              const projectData = await entries.json();
+              //define variables
+              const destination = document.getElementById('destination').value;
+              const departure = document.getElementById('date').value;
+
+              // testing displaying geo API
+              const geoAPI = await retrieveDestination(destination);
+
+              userData.lat = geoAPI.lat;
+              console.log(userData.lat);
+
+              userData.long = geoAPI.long;
+              console.log(userData.long);
+
+              userData.ctry = geoAPI.ctry;
+            }
+          //catch any potential errors that arise and output results in console
+          catch(err){
+             console.log('Error posting data ' + err);
+      }}
+
 export {generateTrip}
