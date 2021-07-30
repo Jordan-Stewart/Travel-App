c1d366203ce3165433834df7293d3bfbf9a1a55a
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-07-30 18:53:40 +1000

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -12,34 +12,6 @@ import './styles/header.scss'
 import './styles/slideshow.scss'
 import './styles.responsive.scss'
 
-let userData = {};
-
-//post data
-const userView = async()=>{
-      const entries = await fetch('/all');
-          //try
-          try{
-              const projectData = await entries.json();
-              //define variables
-              const destination = document.getElementById('destination').value;
-              const departure = document.getElementById('date').value;
-
-              // testing displaying geo API
-              const geoAPI = await retrieveDestination(destination);
-
-              userData.lat = geoAPI.lat;
-              console.log(userData.lat);
-
-              userData.long = geoAPI.long;
-              console.log(userData.long);
-
-              userData.ctry = geoAPI.ctry;
-            }
-          //catch any potential errors that arise and output results in console
-          catch(err){
-             console.log('Error posting data ' + err);
-      }}
-
 
 // Event listener to add function to existing HTML DOM element
 
