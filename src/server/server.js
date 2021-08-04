36605a6a1eb25a4d4ea82f3c2cee65c6358c9b9e
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-04 11:23:40 +1000

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -3,4 +3,4 @@ const server = app.listen(8080, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`Server is running on http://${host}:${port}`);
-}
+})
