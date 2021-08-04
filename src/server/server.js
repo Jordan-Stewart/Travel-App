f3a2797e47b6fee8d6e7366183ce4f430584dd2a
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-04 11:26:11 +1000

    Delete server.js

--- src/server/server.js
+++
@@ -1,6 +0,0 @@
-// Initalize Server
-const server = app.listen(8080, function() {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log(`Server is running on http://${host}:${port}`);
-})
