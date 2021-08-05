c3847688e642e7e87aa5beeb2633b6245c4a853f
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-05 23:37:10 +1000

    removing references to old .js files

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,3 @@
-import { retrieveDestination } from "./js/retrieveDestination";
-import { retrieveImage } from "./js/retrieveImage";
-import { retrieveWeather } from "./js/retrieveWeather";
 import { generateTrip } from './js/app'
 
 import './styles/main.scss'
@@ -10,10 +7,6 @@ import './styles/header.scss'
 import './styles/responsive.scss'
 import './styles/grid.scss'
 
-
-// Event listener to add function to existing HTML DOM element
-
-document.getElementById('generate').addEventListener("click", generateTrip);
 
 //alert("I EXIST")
 //onsole.log("CHANGE!!");
