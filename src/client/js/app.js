04441cc3eedd54533e053a5337a85c35a7ef23eb
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-09 17:32:00 +1000

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -22,65 +22,94 @@ let date = new Date();
 //when generate button is clicked, run generateTrip function
 //document.getElementById('generate').addEventListener('click', generateTrip);
 
-document.getElementById('generate').addEventListener('click', validation);
+document.getElementById('generate').addEventListener('click', generateTrip);
 
-
+var regEx = /^[A-Za-z]+$/;
 //fix generateTrip function as per mentor advice -- https://knowledge.udacity.com/questions/649604
 function generateTrip(e){
-    e.preventDefault();
-    //retrieve elements entered by user
-    //update to include both departure and arrival
-    let arrivalDate = document.getElementById('arrival').value;
-    let departureDate = document.getElementById('departure').value;
-    let destination = document.getElementById('location').value;
-    //determine if forcast will be set for tomorrow or future set date
-    //if (date <= 1) {
-    //  weatherbit_API = todays_forecast;
-    //} else {
-    //  weatherbit_API = future_forecast;
-    //}
-
-    //call retrieveDestination function
-    //updated chaining of function
-    retrieveDestination(destination).then(function(data) {
-      //upon succesful call, call retrieveWeather function
-      retrieveWeather(weatherbit_API, data.geonames[0].lat, data.geonames[0].lng, weatherbit_ID).then(function(data) {
-         //upon succesful calls, call retrieveImage function
-         retrieveImage (pixabay_API, pixabay_ID, destination).then(function (data) {
-            //post data
-            postData('/', {data: data.data, destination: destination, date: date, countdown: countdown}).then(() => {
-                //call userview function
-                userView();
-            });
-         });
-      });
-    });
-}
-
-
-//validate user input and display appropiate error message if input fields are left blank
-function validation() {         
-    if (
-    arrival.value === '' || 
-    arrival.value == null || 
-    departure.value === '' || 
-    departure.value == null || 
-    arrival.value > departure.value ||
-    destination.value === '' || 
-    destination.value == null) 
+      if (
+        destination.value === '' ||
+        destination.value == null )
         {
-        const validateInput = 'Please insert a valid arrival/departure date or your desired destination';
+        const validateInput = 'Please enter your desired destination';
         document.querySelector('#error').innerHTML = validateInput;
-        return false;
-    } else {
-        generateTrip();
-        return true;
-    }
+
+        alert(validateInput);
+
+      } else if (
+          //validation for empty inputs
+        arrival.value === '' ||
+        arrival.value == null ||
+        departure.value === '' ||
+        departure.value == null ||
+        arrival.value > departure.value)
+          {
+          const validateInput = 'Please insert a valid arrival/departure date';
+          document.querySelector('#error').innerHTML = validateInput;
+
+          alert(validateInput);
+
+      } //else if (
+        //validation for letters only in country inputs
+        //destination.value.match(/abc|def/))
+        //{
+        //const validateInput = 'You can only enter letters for country input';
+        //document.querySelector('#error').innerHTML = validateInput;
+
+        //alert(validateInput);
+
+       else {
+          console.log('no validation errors');
+          //clear error message if one is currently displayed
+          document.querySelector('#error').innerHTML = '';
+          //retrieve elements entered by user
+          //update to include both departure and arrival
+          let arrivalDate = document.getElementById('arrival').value;
+          let departureDate = document.getElementById('departure').value;
+          let destination = document.getElementById('location').value;
+          //determine if forcast will be set for tomorrow or future set date
+          //if (date <= 1) {
+          //  weatherbit_API = todays_forecast;
+          //} else {
+          //  weatherbit_API = future_forecast;
+          //}
+
+          //call retrieveDestination function
+          //updated chaining of function
+          retrieveDestination(destination).then(function(data) {
+            //upon succesful call, call retrieveWeather function
+            retrieveWeather(weatherbit_API, data.geonames[0].lat, data.geonames[0].lng, weatherbit_ID).then(function(data) {
+               //upon succesful calls, call retrieveImage function
+               retrieveImage (pixabay_API, pixabay_ID, destination).then(function (data) {
+
+                 // Now pixabay data is available
+                  let userData = {
+                      destinationLattitude: data.geonames[0].lat,
+                      destinationLongitude: data.geonames[0].lng,
+                      destinationCountry: data.geonames[0].countryName,
+                      destinationLow_temp: data.data[0].low_temp,
+                      destinationHigh_temp: data.data[0].high_temp,
+                      weatherDescription: data.data[0].description,
+                      destinationImage: data.hits[0].webformatURL,
+                  };
+
+                  console.log(userData);
+
+                  //post data
+                  postData('/add', userData).then(() => {
+                      //call userview function
+                      userView();
+                  });
+               });
+            });
+          });
+      }
+
 }
 
 //retrieveDestination function
 const retrieveDestination = async (destination) => {
-      const response = await fetch(geonames_API + destination + geonames_ID)
+      const response = await fetch(geonames_API + destination + "&maxRows=10&username=" + geonames_ID)
       try {
           const data = await response.json(); // Return data as JSON
           return data;
@@ -96,11 +125,11 @@ const retrieveImage = async (pixabay_API, pixabay_ID, destination) => {
       const response = await fetch(pixabay_API + pixabay_ID + '&q=' + destination + '&category=places&image_type=photo')
       try {
           const data = await response.json(); // Return data as JSON
-          
+
           //testing display of image
-          const testdata = res.hits[1];
-          console.log(testdata);
-          
+          //const testdata = res.hits[1];
+          //console.log(testdata);
+
           return data;
           }
           //catch any potential errors that arise and output results in console
@@ -126,7 +155,7 @@ const retrieveWeather = async (weatherbit_API, lat, lng, weatherbit_ID) => {
 //Displaying final outcome of destination data
 const postData= async ( url = '', data = {})=>{
       //all of the following has been copied across from my third project
-      const response = await fetch('http://localhost:8080', {
+      const response = await fetch('http://localhost:8081', {
       //post data
       method: 'POST',
       //set credentials
@@ -155,6 +184,7 @@ const userView = async()=>{
       const entries = await fetch('/all');
           try{
               const projectData = await entries.json();
+              console.log(projectData);
               //retrieve icon
               //const icon = document.getElementById("destination")
               //const icon = document.getElementById("arrival")
@@ -181,22 +211,23 @@ const userView = async()=>{
               var difference_time = setDate.getTime() - todaysDate.getTime();
               // To calculate the no. of days between two dates
               const countdown = difference_time / (1000 * 3600 * 24);
+
               //retrieve html i.d's
               document.querySelector('#destination').innerHTML, document.querySelector('#countdown').innerHTML = "In " + countdown + " days you'll be heading off to " + projectData.location;
               document.querySelector('#duration').innerHTML = "The length of your trip is " + duration_length + " days";
-              document.querySelector('#temp').innerHTML = "The temperature is currently: " + projectData.temp;
-              document.querySelector('#high_temperature').innerHTML= "The current forecasted max temperature is " + projectData.maxTemp + " degrees";
-              document.querySelector('#low_temperature').innerHTML= "The current forecasted minimum temperature is " + projectData.minTemp + " degrees";
-              document.querySelector('#weather_description').innerHTML= projectData.weatherDesc;
-
+              document.querySelector('#high_temperature').innerHTML= "The current forecasted max temperature is " + projectData.destinationHigh_temp + " degrees";
+              document.querySelector('#low_temperature').innerHTML= "The current forecasted minimum temperature is " + projectData.destinationLow_temp + " degrees";
+              document.querySelector('#weather_description').innerHTML= projectData.weatherDescription;
+              document.querySelector('#image').src = projectData.destinationImage;
               //Display images/icons
               //Check if country image can be found, if so; display
-              countryImage.setAttribute("src", projectData.countryArray[0]);    
-              if(projectData.countryArray === undefined){
-               document.querySelector('#country_error ').innerHTML= "No valid image could be found";
-              } else {
-                countryImage.setAttribute("src", projectData.countryArray[0]);   
-              }
+
+              ///countryImage.setAttribute("src", projectData.countryArray[0]);
+              //if(projectData.countryArray === undefined){
+              // document.querySelector('#country_error ').innerHTML= "No valid image could be found";
+              //} else {
+              //  countryImage.setAttribute("src", projectData.countryArray[0]);
+              //}
               //display icon
               icon.setAttribute("src", `https://www.weatherbit.io/static/img/icons/${projectData.weatherIcon}.png`);
 
