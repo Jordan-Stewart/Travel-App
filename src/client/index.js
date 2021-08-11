bba0d0bb71d8dc058d210adf537f1a8f828499d1
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-11 15:14:55 +1000

    final changes

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,11 @@
 import { generateTrip } from './js/app'
 
+import "./media/weather_placeholder.png";
+import "./media/placeholder_country.jpg";
+
 import './styles/main.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-
-import './styles/responsive.scss'
 import './styles/grid.scss'
-
-
-//alert("I EXIST")
-//onsole.log("CHANGE!!");
+import './styles/responsive.scss'
