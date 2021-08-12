89dd6e4a2b057c98e87faee048c54a301d72885e
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-12 17:20:50 +1000

    Update testApp.test.js

--- _test_/testApp.test.js
+++ _test_/testApp.test.js
@@ -1,7 +1,7 @@
 // Import the js file to test
-import  { validateText } from "../src/client/js/validateText"
+import  { validateText } from '../src/client/js/validateText'
 
-test('validate strings', () => {
+test('validate text', () => {
            expect(validateString("")).toBe(false);
            expect(validateString("test")).toBe(true);
     });
