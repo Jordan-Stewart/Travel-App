8fc709b7c938de727efdb631cf59345a8dae7ff7
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-12 14:31:41 +1000

    Update testApp.test.js

--- _test_/testApp.test.js
+++ _test_/testApp.test.js
@@ -1,5 +1,5 @@
 // Import the js file to test
-import  { validateString } from "../src/client/js/validateString"
+import  { validateText } from "../src/client/js/validateText"
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.
 // A test suite may contain one or more related tests
 test('validate strings', () => {
