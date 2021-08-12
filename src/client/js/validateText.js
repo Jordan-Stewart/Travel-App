5096975f6e819dff33ccd3fd2afd2dbd52719ddb
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-12 14:31:28 +1000

    Update and rename validateString.js to validateText.js

--- src/client/js/validateText.js
+++ src/client/js/validateText.js
@@ -1,4 +1,4 @@
 //for testing - sourced from https://knowledge.udacity.com/questions/252297
-export function validateString(str) {
+export function validateText(str) {
   return str.length > 0 ? true : false
 }
