b0ba0f9593606225dee86c40e53584873e30d594
Jordan Stewart <85596855+Jordan-Stewart@users.noreply.github.com>
2021-08-12 17:20:24 +1000

    Update testApp.test.js

--- _test_/testApp.test.js
+++ _test_/testApp.test.js
@@ -1,12 +1,7 @@
 // Import the js file to test
 import  { validateText } from "../src/client/js/validateText"
-// The describe() function takes two arguments - a string description, and a test suite as a callback function.
-// A test suite may contain one or more related tests
+
 test('validate strings', () => {
-           // Define the input for the function, if any, in the form of variables/array
-           // Define the expected output, if any, in the form of variables/array
-           // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-           // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
            expect(validateString("")).toBe(false);
            expect(validateString("test")).toBe(true);
     });
